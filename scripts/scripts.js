b0afcdf3cc2a322ccb2a23a2487fb84f8e77f087
Frost1801 <sergio.cibecchini@gmail.com>
2023-04-15 23:05:41 +0200

    Added searchbar (still need media queries)

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,26 @@
 let hamb = document.getElementById("h-button");
+let search = document.getElementById("eyeglass");
 let activeHamburger = false;
+let activeSearchbar = false;
+
 hamb.addEventListener("click", function() {
     if (!activeHamburger){
-        document.getElementById("h-menu").style.display = "block";
+        document.getElementById("d-menu").style.display = "block";
         activeHamburger = true;
     }
     else {
-        document.getElementById("h-menu").style.display = "none";
+        document.getElementById("d-menu").style.display = "none";
         activeHamburger = false;
+    }
+});
+
+search.addEventListener("click", function() {
+    if (!activeSearchbar){
+        document.getElementById("search-bar").style.display = "inline";
+        activeSearchbar = true;
+    }
+    else {
+        document.getElementById("search-bar").style.display = "none";
+        activeSearchbar = false;
     }
 });
